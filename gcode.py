17523ef79ebfc025c63eb84aba09687a412adeaf
andreaspohl <andreaspohl@users.noreply.github.com>
2023-01-02 15:47:28 +0100

    feat: add normalize

--- gcode.py
+++ gcode.py
@@ -7,10 +7,9 @@ from gcode_pos import Gcode_pos as gp
 
 class Gcode:
 
-    lines = None # the content of the gcode file
-    bookmark = 0 # line to read next
-    size = 0 # number of lines
-    cmds = list(()) # the (eventually normalized) list of machine commands aka positions
+    def __init__(self) -> None:
+        self.lines = None # the content of the gcode file
+        self.cmds = list(()) # the (eventually normalized) list of machine commands aka positions
 
     def parse_test(self):
         self.lines = [
@@ -18,6 +17,14 @@ class Gcode:
             'G1 X100.00 Y100.00',
             '',
             'G1 X200.00 Y0.00']
+        self.size = len(self.lines)
+
+    def parse_test2(self):
+        self.lines = [
+            'G1 X-0.10 Y0.00',
+            'G1 X99.90 Y100.00',
+            '',
+            'G1 X199.90 Y0.00']
         self.size = len(self.lines)
 
     # checks if the gcodes fit into the canvas
@@ -36,9 +43,32 @@ class Gcode:
             if pos is not None:
                 self.cmds.append(pos)
 
-    # calculates factor to fit height into 0..1000
+    # normalizes all x,y commands to fit the whole command list exactly into 0..1000
     def normalize(self):
-        print('tbd')
+        x_min = None
+        x_max = None
+        y_min = None
+        y_max = None
+
+        for cmd in self.cmds:
+            if x_min is None:
+                x_min = cmd.x
+                x_max = cmd.x
+                y_min = cmd.y
+                y_max = cmd.y
+            else:
+                x_min = min(x_min, cmd.x)
+                x_max = max(x_max, cmd.x)
+                y_min = min(y_min, cmd.y)
+                y_max = max(y_max, cmd.y)
+        
+        delta_x = (x_max - x_min) / 1000
+        delta_y = (y_max - y_min) / 1000
+
+        for cmd in self.cmds:
+            cmd.x = (cmd.x - x_min) / delta_x
+            cmd.y = (cmd.y - y_min) / delta_y
+        print('wait')
 
     # decodes a line in the nc file and returns a position (machine command)
     # G1 X4.64 Y6.04 --> pen is down, x=4.64, y=6.04
@@ -63,28 +93,6 @@ class Gcode:
                     raise Exception('Unknown gcode: ' + cmd)
                     break
         return pos
-
-    # resets to first line in gcode
-    def reset(self):
-        self.bookmark = 0
-    
-    # gets the next gcode command
-    # returns the next machine position
-    # deprecated
-    def get_next(self):
-        pos = gp()
-
-        if self.bookmark == self.size:
-            pos = None
-        else:
-            command = self.lines[self.bookmark]
-            self.bookmark += 1
-            sub_cmds = command.split(' ')
-            for sub_cmd in sub_cmds:
-                decoded = self.decode(sub_cmd)
-            
-        return (pos)
-
 
 # ---------------------------------------------------------
 import unittest
@@ -115,6 +123,35 @@ class Test(unittest.TestCase):
                 self.assertEqual(gc.cmds[1], gp(True, 100.0, 100.0))
             elif i == 2:
                 self.assertEqual(gc.cmds[2], gp(True, 200.0, 0.0))
+
+    def test_normalize(self):
+        gc = Gcode()
+        gc.parse_test2()
+        gc.create_cmd_list()
+        gc.normalize()
+        for i in range(len(gc.cmds)):
+            if i == 0:
+                self.assertEqual(gc.cmds[0], gp(True, 0.0, 0.0))
+            elif i == 1:
+                self.assertEqual(gc.cmds[1], gp(True, 500.0, 1000.0))
+            elif i == 2:
+                self.assertEqual(gc.cmds[2], gp(True, 1000.0, 0.0))
+    
+    def test_print(self):
+        gc = Gcode()
+        gc.parse_test2()
+        gc.create_cmd_list()
+        gc.normalize()
+
+        print('Lines:')
+        for line in gc.lines:
+            print(line)
+        print()
+
+        print('Commands:')
+        for cmd in gc.cmds:
+            print(cmd.pen_down, cmd.x, cmd.y)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)    
